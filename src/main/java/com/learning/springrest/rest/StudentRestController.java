7d5109ad2c9aa46a05271f27709842a8b305ae29
Tarun Chopra <tchopra@catalystone.com>
2020-05-24 21:15:40 +0530

    Implemented @ControllerAdvice

--- src/main/java/com/learning/springrest/rest/StudentRestController.java
+++ src/main/java/com/learning/springrest/rest/StudentRestController.java
@@ -6,12 +6,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import com.learning.springrest.entity.Student;
-import com.learning.springrest.entity.StudentErrorResponse;
 import com.learning.springrest.entity.StudentNotFoundException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,27 +38,5 @@ public class StudentRestController {
     public Student getStudents(@PathVariable("id") int id) {
         return students.stream().filter(s -> s.getId() == id).findFirst()
                 .orElseThrow(() -> new StudentNotFoundException("Student with id = " + id + ", not found."));
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
