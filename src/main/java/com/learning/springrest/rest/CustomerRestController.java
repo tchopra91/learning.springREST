40a520a70882a7fa748a59575cace4d2ffe891e1
Tarun Chopra <tchopra@catalystone.com>
2020-05-25 19:00:14 +0530

    Implemented REST endpoint to fetch a of customer by id from database

--- src/main/java/com/learning/springrest/rest/CustomerRestController.java
+++ src/main/java/com/learning/springrest/rest/CustomerRestController.java
@@ -7,6 +7,7 @@ import com.learning.springrest.service.CustomerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer getCustomers(@PathVariable int id) {
+        return customerService.getCustomer(id);
     }
 }
