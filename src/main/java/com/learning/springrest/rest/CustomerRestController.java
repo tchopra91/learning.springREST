0e1502de7a4d5f37198aa7bbbe3ccc21a86db113
Tarun Chopra <tchopra@catalystone.com>
2020-05-28 20:54:21 +0530

    Implemented REST endpoint to delete a customer from database

--- src/main/java/com/learning/springrest/rest/CustomerRestController.java
+++ src/main/java/com/learning/springrest/rest/CustomerRestController.java
@@ -8,6 +8,7 @@ import com.learning.springrest.entity.CustomerNotFoundException;
 import com.learning.springrest.service.CustomerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,7 @@ public class CustomerRestController {
     }
 
     @GetMapping("/customers/{id}")
-    public Customer getCustomers(@PathVariable int id) {
+    public Customer getCustomer(@PathVariable int id) {
         Customer customer = customerService.getCustomer(id);
 
         if (Objects.isNull(customer)) {
@@ -53,5 +54,18 @@ public class CustomerRestController {
         customerService.saveCustomer(customer);
 
         return customer;
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public String deleteCustomer(@PathVariable int id) {
+        Customer customer = customerService.getCustomer(id);
+
+        if (Objects.isNull(customer)) {
+            throw new CustomerNotFoundException("Customer with id = " + id + ", not found");
+        }
+
+        customerService.deleteCustomer(id);
+
+        return "Deleted customer with id :: " + id;
     }
 }
